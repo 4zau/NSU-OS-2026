7a827f913e4057abf046e540aff0c6098cfea6ce
Evgenii Sapronov <e.sapronov@g.nsu.ru>
2026-09-07 14:09:16 +0700

    daytime savings

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+
 extern char *tzname[];
 
 int main()
@@ -9,17 +10,18 @@ int main()
     time_t now;
     struct tm *sp;
 
+    putenv("TZ=PST8PDT");
+    tzset();
+    
     (void) time( &now );
 
-    now = now - 8 * 60 * 60;
-
-    printf("%s", ctime( &now ) );
+    printf("%s", ctime(&now));
 
     sp = localtime(&now);
 
     printf("%d/%d/%02d %d:%02d %s\n",
         sp->tm_mon + 1, sp->tm_mday,
-        sp->tm_year, sp->tm_hour,
+        sp->tm_year % 100, sp->tm_hour,
         sp->tm_min, tzname[sp->tm_isdst]);
     
     exit(0);
