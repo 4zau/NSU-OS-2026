75009b351a56f972ba4c4cb08282c388d6c89f0a
Evgenii Sapronov <e.sapronov@g.nsu.ru>
2026-09-07 12:12:25 +0700

    extime

--- main.c
+++ main.c
@@ -1,3 +1,26 @@
-int main() {
-    return 0;
+#include <sys/types.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+extern char *tzname[];
+
+int main()
+{
+    time_t now;
+    struct tm *sp;
+
+    (void) time( &now );
+
+    now = now - 8 * 60 * 60;
+
+    printf("%s", ctime( &now ) );
+
+    sp = localtime(&now);
+
+    printf("%d/%d/%02d %d:%02d %s\n",
+        sp->tm_mon + 1, sp->tm_mday,
+        sp->tm_year, sp->tm_hour,
+        sp->tm_min, tzname[sp->tm_isdst]);
+    
+    exit(0);
 }
